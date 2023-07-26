12a112bcffc6b1b54765d62a53dc921ce8d0857a
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-07-26 17:29:02 +0800

    add: jestCLI

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,18 +1,18 @@
 import {sum} from '../src/sum';
 
-test('test sum(1,2) must be 3', () => {
+test('test sum 1 & 2 must be 3', () => {
   const result = sum(1,2);
 
   expect(result).toBe(3)
 });
 
-test('test sum(2,3) must be 5', () => {
+test('test sum 2 & 3 must be 5', () => {
   const result = sum(2,3);
 
   expect(result).toBe(5)
 });
 
-test('test sum(1,1) must be 1', () => {
+test('test sum 1 & 1 must be 2', () => {
   const result = sum(1,1);
 
   expect(result).toBe(2)
