76aa6daf994cd7c6622b288c6a38de13aeb65358
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-07-26 17:15:52 +0800

    add: jest configuration

--- test/sum.test.js
+++ test/sum.test.js
@@ -5,3 +5,15 @@ test('test sum(1,2) must be 3', () => {
 
   expect(result).toBe(3)
 });
+
+test('test sum(2,3) must be 5', () => {
+  const result = sum(2,3);
+
+  expect(result).toBe(5)
+});
+
+test('test sum(1,1) must be 1', () => {
+  const result = sum(1,1);
+
+  expect(result).toBe(2)
+});
