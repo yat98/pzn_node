cc0efeb9f7b3337f58a62d82b1699ca828b09d35
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-07-28 15:19:12 +0800

    add: mock function

--- src/sum.js
+++ src/sum.js
@@ -7,3 +7,19 @@ export const sumAll = (numbers) => {
   }
   return total;
 }
+
+export const calculate = (numbers,callback) => {
+  let total = 0;
+  for(let number of numbers){
+    total += number;
+  }
+  callback(total);
+}
+
+export const calculateAndReturn = (numbers,callback) => {
+  let total = 0;
+  for(let number of numbers){
+    total += number;
+  }
+  return callback(total);
+}
