868e6213d07d66aabf1b9d4bae8e73da87adf466
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-07-30 20:33:40 +0800

    add: transport

--- test/logging.test.js
+++ test/logging.test.js
@@ -1,10 +1,14 @@
 import winston from "winston";
 
 test('create new logger', () => {
-  const logger = winston.createLogger({});
+  const logger = winston.createLogger({
+    transports: [
+      new winston.transports.Console({}),
+    ],
+  });
 
   logger.log({
-    level: 'log',
+    level: 'info',
     message: 'Hello logger'
   });
 });
