d07f8c282dd032e91625113eb480777836f8216c
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-08-09 18:49:45 +0800

    add: mustache section data

--- test/mustache_section.test.js
+++ test/mustache_section.test.js
@@ -1,7 +1,7 @@
 import Mustache from 'mustache';
 import fs from 'fs/promises';
 
-test('test mustache file', async () => {
+test('test mustache section', async () => {
   const helloTemplate = await fs.readFile('./templates/person.mustache')
     .then(data => data.toString());
 
@@ -13,10 +13,22 @@ test('test mustache file', async () => {
   expect(data).toContain('Hello Person');
 });
 
-test('test mustache file not', async () => {
+test('test mustache section not', async () => {
   const helloTemplate = await fs.readFile('./templates/person.mustache')
     .then(data => data.toString());
 
   const data = Mustache.render(helloTemplate,{});
   expect(data).not.toContain('Hello Person');
+});
+
+test('test mustache section data', async () => {
+  const helloTemplate = await fs.readFile('./templates/person_data.mustache')
+    .then(data => data.toString());
+
+  const data = Mustache.render(helloTemplate,{
+    person: {
+      name: 'Hidayat'
+    }
+  });
+  expect(data).toContain('Hello Person Hidayat!');
 });
