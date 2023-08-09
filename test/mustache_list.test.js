77dc8fe4036e25f4363da9df12c37aeddf1878b2
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-08-09 19:18:34 +0800

    add: mustache list object

--- test/mustache_list.test.js
+++ test/mustache_list.test.js
@@ -18,3 +18,22 @@ test('test mustache list', async () => {
   expect(data).toContain('Gaming');
   expect(data).toContain('Reading');
 });
+
+test('test mustache list object', async () => {
+  const helloTemplate = await fs.readFile('./templates/students.mustache')
+    .then(data => data.toString());
+
+  const data = Mustache.render(helloTemplate,{
+    students: [
+      {name: 'John', value: 88},
+      {name: 'James', value: 92},
+      {name: 'Lia', value: 81},
+    ]
+  });
+  expect(data).toContain('John');
+  expect(data).toContain('88');
+  expect(data).toContain('James');
+  expect(data).toContain('92');
+  expect(data).toContain('Lia');
+  expect(data).toContain('81');
+});
