027f1e8ada4d1694a9242d29ff11df89c4411a23
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-08-09 18:21:50 +0800

    add: mustache tags

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -10,3 +10,8 @@ test('test mustache cache', () => {
   const data = Mustache.render('Hello, {{name}}',{name: 'Hidayat'});
   expect(data).toBe('Hello, Hidayat');
 });
+
+test('test mustache tag', () => {
+  const data = Mustache.render('Hello, {{name}}, my hobby is {{{hobby}}}',{name: 'Hidayat', hobby: '<b>Programming</b>'});
+  expect(data).toBe('Hello, Hidayat, my hobby is <b>Programming</b>');
+});
