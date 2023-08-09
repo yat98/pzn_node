af9038e72f161a9d543aba22343e695f073efb10
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-08-09 17:45:48 +0800

    init: mustache

--- test/array.test.js
+++
@@ -1,9 +0,0 @@
-test('array', () => {
-  const names = ['Hidayat', 'Chandra'];
-  expect(names).toContain('Hidayat');
-  expect(names).toEqual(['Hidayat', 'Chandra']);
-
-  const persons = [{name: 'hidayat'}, {name:'chandra'}];
-  expect(persons).toContainEqual({name: 'chandra'});
-  expect(persons).toEqual([{name: 'hidayat'}, {name:'chandra'}]);
-});
