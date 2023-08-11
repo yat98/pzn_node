2167c64cf20e35b1e964cb861b0dce00fc2f3763
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-08-11 17:06:51 +0800

    add: one to many relation

---
+++ test/prisma_one_to_many.test.js
@@ -0,0 +1,61 @@
+const { prismaClient } = require("../src/prisma_client");
+
+describe('Prisma client', () => {
+  it('should be create data many to one', async() => {
+    const comment = await prismaClient.comment.create({
+      data: {
+        customer_id: 'josh',
+        title: 'Comment 4',
+        description: 'Sample Comment 4',
+      },
+      include: {
+        customer: true
+      }
+    });
+
+    console.info(comment);
+  });
+  it('should be create data one to many', async() => {
+    const customer = await prismaClient.customer.create({
+      data: {
+        id: 'alex',
+        name: 'Alex',
+        email: 'alex@mail.com',
+        phone: '0899999999',
+        comments: {
+          createMany: {
+            data: [
+              {title: 'Comment 1', description: 'Sample Comment 1'},
+              {title: 'Comment 2', description: 'Sample Comment 2'},
+              {title: 'Comment 3', description: 'Sample Comment 3'},
+            ]
+          }
+        }
+      },
+      include: {
+        comments: true
+      }
+    });
+
+    console.info(customer);
+  });
+  it('should be findMany', async() => {
+    const customer = await prismaClient.customer.findMany({
+      where: {
+        comments: {
+          none: {
+            title: {
+              contains: 'Comment'
+              // equals: 'Comment 1'
+            }
+          }
+        }
+      },
+      include: {
+        comments: true
+      }
+    });
+
+    console.info(JSON.stringify(customer));
+  });
+})
